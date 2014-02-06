e577877da77c5727b7b946c936bf2c44519cbb50
rjiwani <rjiwani2014@kellogg.northwestern.edu>
2014-02-05 22:58:05 -0600

    Homework 4
    Homework 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Week4hw::Application.routes.draw do
 
   get "/" => "pages#home"
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
+
+  get "/show" => "products#show"
+  get "/index" => "products#index"
 
   # HINT: Add more routes here...
 
